37b9f674e04a9ee26d24195f0af491e9ef6c59da
Edipo de Almeida <edipo.informatica@gmail.com>
2021-05-31 20:25:45 -0300

    execução programa

---
+++ execucao_classes_filme_series.py
@@ -0,0 +1,11 @@
+from Classes_filme_series import Filme, Serie
+
+
+filme = Filme("A culpa é das estrelas", 2019, 160)
+
+serie  = Serie ("vikings", 2014, 7)
+
+lista = [filme, serie]
+
+for programas in lista:
+    print (programas)
